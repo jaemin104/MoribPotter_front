ca444933f565d9de3f690244ecfcc43222af07d4
jaemin kim <kjm041107@hanyang.ac.kr>
2025-01-04 13:53:41 +0900

    social login front-back linked

---
+++ android/app/src/main/kotlin/com/example/moribpotter/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.moribpotter
+
+import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Google Sign-In 코드 작성
+    }
+}
